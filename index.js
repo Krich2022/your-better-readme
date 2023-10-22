4d16eaad296f962361604e848f781f321f72f8a5
Krich2020 <keston@therichfamily.net>
2023-10-22 10:24:35 -0600

    Created questions to promt the user, added inquirer and fs npm packages, created file writing function.' '

--- index.js
+++ index.js
@@ -1,14 +1,81 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
+import generateMarkdown from "../utils/generateMarkdown";
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    name: "user",
+    message: "What is your GitHub username?",
+    type: "input",
+  },
+  {
+    name: "email",
+    message: "What is your email?",
+    type: "input",
+  },
+  {
+    name: "title",
+    message: "What is the name of your project?",
+    type: "input",
+  },
+  {
+    name: "desc",
+    message: "Write a short description of your project.",
+    type: "input",
+  },
+  {
+    name: "license",
+    message: "What kind of license should your project have?",
+    type: "list",
+    choices: [
+      "MIT License",
+      "GNU AGPLv3",
+      "GNU GPLv3",
+      "GNGU LGPLv3",
+      "Mozilla Public License 2.0",
+      "Apache License 2.0",
+      "Boost Software License 1.0",
+      "The Unlicense",
+    ],
+  },
+  {
+    name: "install",
+    message: "What command should be used to install dependencies?",
+    type: "input",
+  },
+  {
+    name: "test",
+    message: "What command should be used to test?",
+    type: "input",
+  },
+  {
+    name: "repo",
+    message: "What should the user know about using the repo?",
+    type: "input",
+  },
+  {
+    name: "contribute",
+    message: "What should the user know about contributing to the repo?",
+    type: "input",
+  },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) throw err;
+    console.log(`File ${fileName} created successfully!`);
+  });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+const init = async () => {
+  const data = await inquirer.prompt(questions);
+  const markdown = generateMarkdown(data);
+  console.log(data);
+  console.log(markdown);
+};
 
 // Function call to initialize app
 init();
